c790c6f6342d8d92029c997a04934aadef0e1e2a
naeel <naeel.mostafa@student.hu.nl>
2017-09-08 19:36:23 +0200

    dat is les 1 van Naeel Mostafa

---
+++ les1/2.py
@@ -0,0 +1,12 @@
+naeel = ('Supercalifragilisticexpialidocious')
+print(naeel)
+print (naeel[18:21])
+print (len(naeel))
+'Antidisestablishmentarianism'> 'Honorificabilitudinitatibus'
+Naeel=[ 'Berlioz', 'Borodin', 'Brian',
+'Bartok', 'Bellini', 'Buxtehude', 'Bernstein']
+print(Naeel)
+Naeel.sort()
+print(Naeel)
+Naeel.reverse()
+print(Naeel)
---
+++ les1/5.py
@@ -0,0 +1,8 @@
+favorieten= ['Maher']
+print(favorieten)
+favorieten.append('Ali')
+print(favorieten)
+favorieten.remove('Ali')
+print(favorieten)
+favorieten.append('Nancy')
+print(favorieten)
